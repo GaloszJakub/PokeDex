78fd613707007561f6dc45dbed0fc15356fd0737
GaloszJakub <kubagalosz@gmail.com>
2025-03-02 17:17:07 +0100

    Updates

---
+++ src/services/Types.ts
@@ -0,0 +1,43 @@
+//Types + Obrazki
+interface PokemonTypeData {
+	pokemon: {
+		pokemon: {
+			name: string
+			url: string
+		}
+	}[]
+}
+
+interface PokemonDetails {
+	sprites: {
+		other: {
+			'official-artwork': {
+				front_default: string
+			}
+		}
+	}
+	types: {
+		type: {
+			name: string
+		}
+	}[]
+}
+
+export const getPokemonByType = async (type: string) => {
+	try {
+		const typeResponse = await fetch(`https://pokeapi.co/api/v2/type/${type}`)
+		const typeData: PokemonTypeData = await typeResponse.json()
+
+		const firstPokemon = typeData.pokemon[0].pokemon
+
+		const pokemonResponse = await fetch(firstPokemon.url)
+		const pokemonData: PokemonDetails = await pokemonResponse.json()
+
+		return {
+			type: type,
+			image: pokemonData.sprites.other['official-artwork'].front_default,
+		}
+	} catch (error) {
+		throw new Error(`Error fetching ${type} type Pokemon: ${error}`)
+	}
+}
