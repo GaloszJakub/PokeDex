b057e7cfa116f8f7ad250f6cb8ad99657c515f30
GaloszJakub <kubagalosz@gmail.com>
2025-03-10 18:28:13 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
 			className="w-[270px] h-screen fixed left-0 top-0 bg-cover bg-center hidden lg:block z-50">
 			<div className="bg-black/40 absolute w-full h-full inset-0" />
 			<div className="absolute z-20 flex flex-col gap-y-8 mt-20 mx-auto ">
-				<Link to="/PokeDex">
+				<Link to="/">
 					<img
 						src={pokelogo}
 						alt="pokemon logo"
@@ -19,7 +19,7 @@ export default function Navbar() {
 				</Link>
 
 				<div className="space-y-4 px-10">
-					<Link to="/">
+					<Link to="/PokeDexList">
 						<NavbarButton label="Pokedex" />
 						<NavbarButton label="Moves" />
 						<NavbarButton label="Abilities" />
